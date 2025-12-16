a5703045d409612f33632e7e6565202e64677016
immortalab <104135945+immortalab@users.noreply.github.com>
2025-12-17 02:01:18 +0300

    Update socket.ts

--- src/app/socket.ts
+++ src/app/socket.ts
@@ -1,5 +1,9 @@
 import { io } from 'socket.io-client';
 
-const socket = io();
+// Явно указываем адрес вашего сервера на Render
+const socket = io('https://chessbot-zl45.onrender.com', {
+  transports: ['websocket', 'polling'], // Добавляем поддержку разных способов связи
+  withCredentials: true
+});
 
 export default socket;
