232157596c4f7d9bd5289177817a4a334213f6de
immortalab <104135945+immortalab@users.noreply.github.com>
2025-12-17 01:10:03 +0300

    Update core.ts

--- src/core.ts
+++ src/core.ts
@@ -1,12 +1,20 @@
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import cors from "cors"; // Добавили импорт
 
 export const app = express();
 
+app.use(cors()); // РАЗРЕШАЕМ НАЖАТИЯ КНОПОК
 app.use(express.json());
+
 // Enable static files
 app.use(express.static('build'));
 
 export const httpServer = createServer(app);
-export const io = new Server(httpServer, { /* options */ });
+export const io = new Server(httpServer, { 
+    cors: {
+        origin: "*", // Разрешаем сокеты для всех
+        methods: ["GET", "POST"]
+    }
+});
